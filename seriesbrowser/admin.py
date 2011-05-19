5e0cff08dc0a2f812dd7a0c84a46251d1a96c613
Siddhartha Mitra <mitra.siddhartha@gmail.com>
2011-05-18 20:43:38 -0400

    Admin changes

--- seriesbrowser/admin.py
+++ seriesbrowser/admin.py
@@ -3,17 +3,9 @@ from seriesbrowser.models import Series, Section, Injection
 
 class SeriesAdmin(admin.ModelAdmin):
     list_display = ('desc', 'lab','isRestricted', 'numQCSections')
-    list_filter    = ('desc',)
     fields = ('desc', 'lab','isRestricted', 'numQCSections')
     readonly_fields =('desc', 'lab', 'numQCSections')  
 admin.site.register(Series, SeriesAdmin)
-
-# class SectionAdmin(admin.ModelAdmin):
-#     list_display = ('series', 'name','isSampleSection','sectionOrder')
-#     list_filter    = ('name',)
-#     fields = ('series','name','isSampleSection','sectionOrder')
-#     readonly_fields = ('series','name','sectionOrder')
-# admin.site.register(Section, SectionAdmin)
 
 class InjectionAdmin(admin.ModelAdmin):
     list_display = ('series', 'x_coord','y_coord','z_coord','region')
